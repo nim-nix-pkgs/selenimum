1fa3b445d445447c94bb2f7448436d535063eb1e
hugosenari <hugosenari@gmail.com>
2022-03-12 04:02:04 -0300

    chore: re index selenimum

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = ''WebDriver for Selenium(selenium-hub).'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."selenimum-main".url = "path:./main";
+  inputs."selenimum-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."selenimum-v0_1_0".url = "path:./v0_1_0";
+  inputs."selenimum-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."selenimum-v0_1_1".url = "path:./v0_1_1";
+  inputs."selenimum-v0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-v0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."selenimum-v0_1_2".url = "path:./v0_1_2";
+  inputs."selenimum-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."selenimum-v0_1_3".url = "path:./v0_1_3";
+  inputs."selenimum-v0_1_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-v0_1_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."selenimum-v0_2_0".url = "path:./v0_2_0";
+  inputs."selenimum-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."selenimum-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''WebDriver for Selenium(selenium-hub).'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-selenimum-main.flake = false;
+  inputs.src-selenimum-main.owner = "myamyu";
+  inputs.src-selenimum-main.ref   = "refs/heads/main";
+  inputs.src-selenimum-main.repo  = "selenimum";
+  inputs.src-selenimum-main.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-selenimum-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-selenimum-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
